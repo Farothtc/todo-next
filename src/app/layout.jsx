74cc4645b588a9c346f734b667d4b54eb8159687
Farothtc <mvkirimi@gmail.com>
2024-11-22 16:07:03 +0300

    Nav and Footer imported

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,15 @@
+import { Footer } from "./components/Footer";
+import { Nav } from "./components/Nav";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Nav />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
