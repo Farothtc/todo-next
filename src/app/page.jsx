454d95f028c27cb1d23d66ec9397abd0bfdf14e6
Farothtc <mvkirimi@gmail.com>
2024-11-22 18:52:52 +0300

    General task addition implemented

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,40 @@
+"use client";
+import { Task } from "./components/Task";
+import { useState } from "react";
+
 export default function Home() {
+  const [todoText, setTodoText] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const handleClick = () => {
+    if (todoText) {
+      setTasks((prevTask) => [...prevTask, todoText]);
+      setTodoText("");
+    }
+  };
+
   return (
     <>
-      <div>
-        <h1>Hello</h1>
-      </div>
+      <main className="main-all">
+        <div className="container mx-auto">
+          <div className="text-wrapper flex justify-center absolute inset-0 gap-3">
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full max-w-xs entered-text"
+              onChange={(el) => setTodoText(el.target.value)}
+            />
+            <button onClick={handleClick} className="btn btn-outline">
+              Add Task
+            </button>
+          </div>
+          <div className="new-task text-center flex justify-center absolute inset-0 gap-3">
+            {tasks.map((task, index) => (
+              <Task key={index} todoText={task} id={index + 1} />
+            ))}
+          </div>
+        </div>
+      </main>
     </>
   );
 }
